73a2b1a6b8288428220c1b2a19787f62b732e514
Alex <pr9nek@mail.ru>
2024-02-16 16:00:35 +0300

    unit test reverse string (func makePerform)

--- src/components/string/string.test.tsx
+++ src/components/string/string.test.tsx
@@ -1,6 +1,7 @@
 import { swap } from "../../utils/utils";
 import { ElementStates } from "../../types/element-states";
 import { makePerform } from "./utils/utils";
+import { IPerformance } from "./string";
 
 describe('swap function', () => {
     it('works correctly', () => {
@@ -9,4 +10,54 @@ describe('swap function', () => {
         const act = swap([1, 2, 3], 0, 2);
         expect(act).toEqual(expected);
     });
+});
+
+describe('makePerform function', () => {
+    it('correctly reverses a string with even number of characters', async () => {
+        const arr: IPerformance[] = [{ letter: 'a', state: ElementStates.Default }, { letter: 'b', state: ElementStates.Default }];
+        const setPerform = jest.fn();
+        const setLoader = jest.fn();
+        const setPerformed = jest.fn();
+
+        await makePerform(arr, setPerform, setLoader, setPerformed);
+
+        expect(arr[0].letter).toBe('b');
+        expect(arr[1].letter).toBe('a');
+    });
+
+    it('correctly reverses a string with odd number of characters', async () => {
+        const arr: IPerformance[] = [{ letter: 'a', state: ElementStates.Default }, { letter: 'b', state: ElementStates.Default }, { letter: 'c', state: ElementStates.Default }];
+        const setPerform = jest.fn();
+        const setLoader = jest.fn();
+        const setPerformed = jest.fn();
+
+        await makePerform(arr, setPerform, setLoader, setPerformed);
+
+        expect(arr[0].letter).toBe('c');
+        expect(arr[1].letter).toBe('b');
+        expect(arr[2].letter).toBe('a');
+    });
+
+    it('correctly reverses a string with with one character', async () => {
+        const arr: IPerformance[] = [{ letter: 'a', state: ElementStates.Default }];
+        const setPerform = jest.fn();
+        const setLoader = jest.fn();
+        const setPerformed = jest.fn();
+
+        await makePerform(arr, setPerform, setLoader, setPerformed);
+
+        expect(arr[0].letter).toBe('a');
+    });
+
+    it('correctly reverses an empty string', async () => {
+        const arr: IPerformance[] = [];
+        const setPerform = jest.fn();
+        const setLoader = jest.fn();
+        const setPerformed = jest.fn();
+
+        await makePerform(arr, setPerform, setLoader, setPerformed);
+
+        expect(arr).toEqual([]);
+    });
+
 });
